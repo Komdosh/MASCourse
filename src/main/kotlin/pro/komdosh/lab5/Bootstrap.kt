5cae089963379120b7e5a0d83f38b8b10fbda662
Andrey Tabakov <komdosh@yandex.ru>
2019-11-20 09:50:52 +0300

    attach sniffer

--- src/main/kotlin/pro/komdosh/lab5/Bootstrap.kt
+++ src/main/kotlin/pro/komdosh/lab5/Bootstrap.kt
@@ -1,5 +1,6 @@
 package pro.komdosh.lab5
 
+import pro.komdosh.attachSniffer
 import pro.komdosh.createMainContainer
 import pro.komdosh.lab5.agent.ReceiverAgent
 import pro.komdosh.lab5.agent.SenderAgent
@@ -8,6 +9,11 @@ import pro.komdosh.lab5.agent.SenderAgent
 fun main() {
     val cc = createMainContainer(withDummy = false, withGui = false)
 
-    cc.createNewAgent("Sender", SenderAgent::class.java.name, null).start()
-    cc.createNewAgent("Receiver", ReceiverAgent::class.java.name, null).start()
+    val sender = cc.createNewAgent("Sender", SenderAgent::class.java.name, null)
+    val receiver = cc.createNewAgent("Receiver", ReceiverAgent::class.java.name, null)
+
+    attachSniffer(cc, listOf(sender, receiver))
+
+    sender.start()
+    receiver.start()
 }
