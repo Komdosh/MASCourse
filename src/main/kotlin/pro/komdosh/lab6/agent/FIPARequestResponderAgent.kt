f2c4ede2efbae5e9574fc3fa72d7b351c6d210b2
Andrey Tabakov <komdosh@yandex.ru>
2019-11-20 11:55:38 +0300

    refactor fipa response behaviour

--- src/main/kotlin/pro/komdosh/lab6/agent/FIPARequestResponderAgent.kt
+++ src/main/kotlin/pro/komdosh/lab6/agent/FIPARequestResponderAgent.kt
@@ -1,13 +1,10 @@
 package pro.komdosh.lab6.agent
 
 import jade.core.Agent
-import jade.domain.FIPAAgentManagement.FailureException
-import jade.domain.FIPAAgentManagement.NotUnderstoodException
-import jade.domain.FIPAAgentManagement.RefuseException
 import jade.domain.FIPANames
 import jade.lang.acl.ACLMessage
 import jade.lang.acl.MessageTemplate
-import jade.proto.AchieveREResponder
+import pro.komdosh.lab6.behaviour.FIPAResponseBehaviour
 
 class FIPARequestResponderAgent : Agent() {
     override fun setup() {
@@ -16,50 +13,6 @@ class FIPARequestResponderAgent : Agent() {
             MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST),
             MessageTemplate.MatchPerformative(ACLMessage.REQUEST)
         )
-        addBehaviour(object : AchieveREResponder(this, template) {
-            @Throws(NotUnderstoodException::class, RefuseException::class)
-            override fun prepareResponse(request: ACLMessage?): ACLMessage {
-                println("Agent $localName: REQUEST received from ${request!!.sender.name}. Action is ${request.content}")
-                if (checkAction()) {
-                    // We agree to perform the action. Note that in the FIPA-Request
-                    // protocol the AGREE message is optional. Return null if you
-                    // don't want to send it.
-                    println("Agent $localName: Agree")
-                    val agree = request.createReply()
-                    agree.performative = ACLMessage.AGREE
-                    return agree
-                } else {
-                    // We refuse to perform the action
-                    println("Agent $localName: Refuse")
-                    throw RefuseException("check-failed")
-                }
-            }
-
-            @Throws(FailureException::class)
-            override fun prepareResultNotification(
-                request: ACLMessage?,
-                response: ACLMessage?
-            ): ACLMessage {
-                if (performAction()) {
-                    println("Agent $localName: Action successfully performed")
-                    val inform = request!!.createReply()
-                    inform.performative = ACLMessage.INFORM
-                    return inform
-                } else {
-                    println("Agent $localName: Action failed")
-                    throw FailureException("unexpected-error")
-                }
-            }
-        })
-    }
-
-    private fun checkAction(): Boolean {
-        // Simulate a check by generating a random number
-        return Math.random() > 0.2
-    }
-
-    private fun performAction(): Boolean {
-        // Simulate action execution by generating a random number
-        return Math.random() > 0.2
+        addBehaviour(FIPAResponseBehaviour(this, template))
     }
 }
---
+++ src/main/kotlin/pro/komdosh/lab6/behaviour/FIPAResponseBehaviour.kt
@@ -0,0 +1,56 @@
+package pro.komdosh.lab6.behaviour
+
+import jade.core.Agent
+import jade.domain.FIPAAgentManagement.FailureException
+import jade.domain.FIPAAgentManagement.NotUnderstoodException
+import jade.domain.FIPAAgentManagement.RefuseException
+import jade.lang.acl.ACLMessage
+import jade.lang.acl.MessageTemplate
+import jade.proto.AchieveREResponder
+
+class FIPAResponseBehaviour(agent: Agent, template: MessageTemplate) : AchieveREResponder(agent, template) {
+    @Throws(NotUnderstoodException::class, RefuseException::class)
+    override fun prepareResponse(request: ACLMessage): ACLMessage {
+        println("Agent ${agent.localName}: REQUEST received from ${request.sender.name}. Action is ${request.content}")
+        if (checkAction()) {
+            // We agree to perform the action. Note that in the FIPA-Request
+            // protocol the AGREE message is optional. Return null if you
+            // don't want to send it.
+            println("Agent ${agent.localName}: Agree")
+            val agree = request.createReply()
+            agree.performative = ACLMessage.AGREE
+            return agree
+        } else {
+            // We refuse to perform the action
+            println("Agent ${agent.localName}: Refuse")
+            throw RefuseException("check-failed")
+        }
+    }
+
+    @Throws(FailureException::class)
+    override fun prepareResultNotification(
+        request: ACLMessage,
+        response: ACLMessage
+    ): ACLMessage {
+        if (performAction()) {
+            println("Agent ${agent.localName}: Action successfully performed")
+            val inform = request.createReply()
+            inform.performative = ACLMessage.INFORM
+            return inform
+        } else {
+            println("Agent ${agent.localName}: Action failed")
+            throw FailureException("unexpected-error")
+        }
+    }
+
+    private fun checkAction(): Boolean {
+        // Simulate a check by generating a random number
+        return Math.random() > 0.2
+    }
+
+    private fun performAction(): Boolean {
+        // Simulate action execution by generating a random number
+        return Math.random() > 0.2
+    }
+}
+
